5f1e44f2088f4eaecaec78e5425d4d0a9996e6b7
Suyog Nerkar <suyogrnerkar@gmail.com>
2016-11-09 12:49:47 -0500

    Completed 12 Ques, refactoring and commnets remains

--- prj3-sol.hs
+++ prj3-sol.hs
@@ -8,22 +8,29 @@ import Debug.Trace
 -- negative square-root of the discriminant.  Need not handle complex
 -- roots.
 quadraticRoots :: Floating t => t -> t -> t -> (t, t)
-quadraticRoots a b c =
-  undefined
+quadraticRoots a b c = (p_root, n_root)
+  where
+    real =  - b / (2 * a)
+    delta = b * b - 4 * a * c
+    p_root = real + sqrt delta / (2 * a)
+    n_root = real - sqrt delta / (2 * a)
+
 
 -- Problem 2
 -- Return infinite list containing [z, f(z), f(f(z)), f(f(f(z))), ...]
 -- May use recursion.
 iterateFunction :: (a -> a) -> a -> [a]
-iterateFunction f z =
-  undefined
+iterateFunction f a =
+  a : iterateFunction f (f a)
+
 
 -- Problem 3
 -- Using iterateFunction return infinite list containing 
 -- multiples of n by all the non-negative integers.
 -- May NOT use recursion.
 multiples n =
-  undefined
+  (map (\ y -> y * n) (iterateFunction (\ y -> y + 1) 0))
+
 
 -- Problem 4
 -- Use iterateFunction to return an infinite list containing list 
@@ -34,7 +41,8 @@ multiples n =
 -- May NOT use recursion.
 hailstones :: Integral a => a -> [a]
 hailstones n =
-  undefined
+  (iterateFunction (\ hsn -> if even hsn then div hsn 2 else 3 * hsn + 1) n)
+
 
 -- Problem 5
 -- Return length of hailstone sequence starting with n terminating
@@ -42,7 +50,10 @@ hailstones n =
 -- May NOT use recursion.  Can use elemIndex from Data.List
 hailstonesLen :: Integral a => a -> Int
 hailstonesLen n =
-  undefined
+  case elemIndex 1 (hailstones n) of
+    Just counter -> counter + 1
+    Nothing -> 0
+
 
 -- Problem 6
 -- Given a list of numbers, return sum of the absolute difference
@@ -50,7 +61,11 @@ hailstonesLen n =
 -- May NOT use recursion.
 sumAbsDiffs :: Num a => [a] -> a
 sumAbsDiffs numberList =
-  undefined
+  foldr (+) 0 (
+    map (\ num -> abs num) (
+      zipWith (-) numberList (tail numberList)
+    )
+  )
 
 
 -- Problem 7
@@ -60,7 +75,8 @@ sumAbsDiffs numberList =
 -- May NOT use recursion.
 distances :: Floating b => (b, b) -> [(b, b)] -> [b]
 distances pt points =
-  undefined
+  [sqrt((x - fst pt)^2 + (y - snd pt)^2) | (x, y) <- points]
+
 
 -- Problem 8
 -- Given a list of coordinate pairs representing points, return the
@@ -69,13 +85,14 @@ distances pt points =
 -- May NOT use recursion.
 sumLengths :: Floating a => [(a, a)] -> a
 sumLengths pointsList =
-  undefined
+  foldr (+) 0 (zipWith (\ x y -> sqrt((fst y - fst x)^2 + (snd y - snd x)^2)) pointsList (tail pointsList))
+
 
 -- Problem 9
 -- Given a string s and char c, return list of indexes in s where c
 -- occurs
-occurrences s c =
-  undefined
+occurrences s c = elemIndices c s
+
 
 -- A tree of some type t is either a Leaf containing a value of type t,
 -- or it is an internal node (with constructor Tree) with some left
@@ -84,17 +101,26 @@ data Tree t = Leaf t
             | Tree (Tree t) t (Tree t)
 
 -- Problem 10
--- Fold tree to a single value.  If tree is a Tree node, then it's
+-- Fold tree to a single value.
+
+-- If 'tree' is a Tree node, then it's
 -- folded value is the result of applying ternary treeFn to the result
 -- of folding the left sub-tree, the value stored in the Tree node and
--- the result of folding the right sub-tree; if it is a Leaf node,
+-- the result of folding the right sub-tree;
+-- If 'tree' is a Leaf node,
 -- then the result of the fold is the result of applying the unary
 -- leafFn to the value stored within the Leaf node.
 -- May use recursion.
+
+-- For all below: lst: left sub tree , rst: right sub tree, val: node, lf: leaf
+
+-- E.g. >>> foldTree (\ t1 t t2 -> t1+3*t+t2) (\x -> x*2) (Leaf 5)
+-- E.g. >>> foldTree (\ t1 t t2 -> t1+3*t+t2) (\x -> x*2) (InternalNode (Leaf 3) 2 (Leaf 4))
 foldTree :: (t1 -> t -> t1 -> t1) -> (t -> t1) -> Tree t -> t1
-foldTree treeFn leafFn tree =
-  undefined
-  
+foldTree treeFn leafFn (Leaf tree) = leafFn tree
+foldTree treeFn leafFn (Tree lst val rst) =
+  treeFn (foldTree treeFn leafFn lst) val (foldTree treeFn leafFn rst)
+
 -- Problem 11
 -- Return list containing flattening of tree.  The elements of the
 -- list correspond to the elements stored in the tree ordered as per 
@@ -102,8 +128,7 @@ foldTree treeFn leafFn tree =
 -- May NOT use recursion.
 flattenTree :: Tree a -> [a]
 flattenTree tree =
-  undefined
-
+  foldTree (\ lst val rst -> lst ++ [val] ++ rst) (\ lf -> [lf]) tree
 
 -- Problem 12
 -- Given tree of type (Tree [t]) return list which is concatenation
@@ -112,4 +137,4 @@ flattenTree tree =
 -- May NOT use recursion.
 catenateTreeLists :: Tree [a] -> [a]
 catenateTreeLists tree =
-  undefined
+  foldl (++) [] (flattenTree tree)
